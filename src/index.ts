01666b10468dfd2121c3703a1edfc59f864817bb
Andrei Smitko <asmitko@goodleap.com>
2022-07-29 15:56:19 +0300

    live coding bug squashing session

--- src/index.ts
+++ src/index.ts
@@ -1,173 +1,199 @@
 import fs from "fs";
-import inquirer from 'inquirer';
-import { v4 as uuid } from 'uuid';
+import inquirer from "inquirer";
+import { v4 as uuid } from "uuid";
 
 class Student {
-    public id: string;
-    public name: string;
-    public nrClass: number;
+  public id: string;
+  public name: string;
+  public nrClass: number;
 
-    constructor(response: { id?: string; name?: string}, nrClass: number ){
-        this.id = uuid();
-        this.name = response.name || "";
-        this.nrClass = nrClass;
-    }
+  constructor(name: string, nrClass: number) {
+    this.id = uuid();
+    this.name = name || "";
+    this.nrClass = nrClass;
+  }
 }
 
 class Grade {
-    public idStudent: string;
-    public nameStd: string;
-    public grade: number;
+  public id: string;
+  public idStudent: string;
+  public subject: string;
+  public grade: number;
 
-    constructor(idStudent:string, nameStd:string, grade:number){
-        this.idStudent=uuid();
-        this.nameStd=nameStd;
-        this.grade=grade;
-    }
+  constructor(idStudent: string, subject: string, grade: number) {
+    this.id = uuid();
+    this.idStudent = idStudent;
+    this.subject = subject;
+    this.grade = grade;
+  }
 }
 
-const studentArray:Array<Student> = [];
+const studentArray: Array<Student> = [];
 
-async function addStudent (student:Student):Promise<void>{
-    studentArray.push(student);
-    await fs.writeFileSync("students.json", JSON.stringify(studentArray));
-    
+async function addStudent(student: Student): Promise<void> {
+  studentArray.push(student);
+  await fs.writeFileSync("students.json", JSON.stringify(studentArray));
 }
 
 function insertStudent(): void {
-    inquirer.prompt([
-        {
-            type:"input",
-            name:"student_Name",
-            message:"Student name:",
-            validate:(answer)=>{
-                if(answer.length === 0)
-                    return "Please enter the name!"
-                return true;
-            }  
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "student_Name",
+        message: "Student name:",
+        validate: (answer) => {
+          if (answer.length === 0) return "Please enter the name!";
+          return true;
         },
-        {
-            type:"input",
-            name:"class_number",
-            message:"Class number:",
-            validate:(answer)=>{
-                if (answer<=0 || answer>=12){
-                    return "Please enter the class number!"
-                }
-                return true;
-            }
-        }
+      },
+      {
+        type: "input",
+        name: "class_number",
+        message: "Class number:",
+        validate: (answer) => {
+          if (answer <= 0 || answer >= 12) {
+            return "Please enter the class number!";
+          }
+          return true;
+        },
+      },
     ])
-    .then (answers =>{
-        const newStudent:Student = new Student(answers["student_Name"],answers["class_number"]);
-        addStudent(newStudent);
+    .then((answers) => {
+      const newStudent: Student = new Student(
+        answers["student_Name"],
+        answers["class_number"]
+      );
+      addStudent(newStudent);
+      return main();
+    });
+}
+
+function checkExistStudent(name: string): Student | undefined {
+  for (const studentRecord of studentArray) {
+    if (studentRecord.name === name) {
+      return studentRecord;
+    }
+  }
+  return undefined;
+}
+
+async function upgrade() {
+  try {
+    await fs.writeFileSync("grades.json", JSON.stringify(gradeArray));
+    await fs.writeFileSync("students.json", JSON.stringify(studentArray));
+  } catch {
+    console.log("Something wrong!");
+  }
+}
+
+const gradeArray: Array<Grade> = [];
+function gradeStudent(): void {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "std_Name",
+        message: "Student name:",
+        validate: (answer) => {
+          if (answer.length === 0) return "Please enter the name!";
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "std_subject",
+        message: "Subject name:",
+        validate: (answer) => {
+          if (answer.length === 0) return "Please enter the name!";
+          return true;
+        },
+      },
+      {
+        type: "input",
+        name: "grade_nr",
+        message: "Grade:",
+        validate: (answer) => {
+          if (answer <= 1 || answer >= 10) {
+            return "Grade is not valid!";
+          }
+          return true;
+        },
+      },
+    ])
+    .then((answers) => {
+      const searchStudent: Student = checkExistStudent(
+        answers["std_Name"]
+      ) as Student;
+      if (searchStudent === undefined) {
+        console.log("Student doesn't exist!");
+      } else {
+        const newGrade: Grade = new Grade(
+          searchStudent.id,
+          answers["std_subject"],
+          answers["grade_nr"]
+        );
+        gradeArray.push(newGrade);
+        upgrade();
         return main();
-    })
+      }
+    });
 }
 
-
-function checkExistStudent(name:string):Student | undefined {
-    for (const studentRecord of studentArray){
-        if(studentRecord.name === name){
-            return studentRecord;
-        }
-    }
-    return undefined;
-}
-
-async function upgrade(){
-    try{
-        await fs.writeFileSync("grades.json",JSON.stringify(gradeArray));
-        await fs.writeFileSync("students.json", JSON.stringify(studentArray));
-    } catch{
-        console.log("Something wrong!");
-    }
-}
-
-const gradeArray:Array<Grade> = [];
-function gradeStudent():void{
-    inquirer.prompt([
-        {
-            type:"input",
-            name:"std_Name",
-            message:"Student name:",
-            validate:(answer)=>{
-                if(answer.length === 0)
-                    return "Please enter the name!"
-                return true;
-            }  
+function seeStudent(): void {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "std_name",
+        message: "Enter student name:",
+        validate: (answer) => {
+          if (answer.length === 0) return "Please enter the name!";
+          return true;
         },
-        {
-            type:"input",
-            name:"grade_nr",
-            message:"Grade number:",
-            validate:(answer)=>{
-                if(answer<=1 || answer>=10){
-                    return "Grade is not valid!"
-                }
-                return true;
-            }  
-        }
+      },
     ])
-    .then(answers=>{
-        const searchStudent:Student = checkExistStudent(answers["std_Name"]) as Student ;
-        if (searchStudent === undefined){
-            console.log("Student doesn't exist!");
-        } else{
-            const newGrade:Grade = new Grade(searchStudent.id, answers["std_Name"], answers["grade_nr"]);
-            gradeArray.push(newGrade);
-            upgrade();
-            return main();
-        }
-    })
+    .then((answers) => {
+      const nameStd: string = answers["std_name"];
+      const student = checkExistStudent(nameStd);
+      if (student === undefined) {
+        console.log("Student doesn't exist!");
+        return seeStudent();
+      } else {
+        console.log(
+          gradeArray.filter((grade) => grade.idStudent === student.id)
+        );
+        return main();
+      }
+    });
 }
 
-function seeStudent ():void{
-    inquirer.prompt([
-        {
-            type:"input",
-            name:"std_name",
-            message:"Enter student name:",
-            validate:(answer)=>{
-                if(answer.length === 0)
-                    return "Please enter the name!"
-                return true;
-            }
-        }
+function main(): void {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "welcome",
+        message: "Choose an option:",
+        choices: [
+          "Insert a new student.",
+          "Student's grade.",
+          "See a student.",
+          "Exit!",
+        ],
+      },
     ])
-    .then(answers=>{
-        const nameStd:string = answers["std_name"];
-        if (checkExistStudent(nameStd) === undefined){
-            console.log("Student doesn't exist!");
-            return seeStudent();
-        } else{
-            return main();
-        }
-    })
-}
-
-function main():void{
-    inquirer.prompt([
-        {
-            type:"list",
-            name:"welcome",
-            message:"Choose an option:",
-            choices:[ "Insert a new student.","Student's grade.","See a student.","Exit!"]
-        }
-    ]) .then(answer=>{
-        if(answer["welcome"] === "Insert a new student."){
-            insertStudent();
-        }
-        else if(answer["welcome"] === "Student's grade."){
-            gradeStudent();
-        }
-        else if (answer["welcome"]==="See a student."){
-            seeStudent();
-        }
-        else if(answer["welcome"] === "Exist!"){
-            return ;
-        }
-    })
+    .then((answer) => {
+      if (answer["welcome"] === "Insert a new student.") {
+        insertStudent();
+      } else if (answer["welcome"] === "Student's grade.") {
+        gradeStudent();
+      } else if (answer["welcome"] === "See a student.") {
+        seeStudent();
+      } else if (answer["welcome"] === "Exist!") {
+        return;
+      }
+    });
 }
 
 main();
